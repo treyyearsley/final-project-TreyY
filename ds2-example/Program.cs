0fad21353e171ddf466670591f2e1798763494ff
Trey Yearsley <113956012+treyyearsley@users.noreply.github.com>
2024-07-02 09:55:24 -0600

    Update Program.cs

--- ds2-example/Program.cs
+++ ds2-example/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Sets
+{
+    public static void Main()
+    {
+        HashSet<double> lapTimes = new HashSet<double>(); //Create a new set
+        lapTimes.Add(51.3);
+        lapTimes.Add(48.5);
+        lapTimes.Add(49.2);
+        lapTimes.Add(46.3);
+        lapTimes.Add(47.8);
+        lapTimes.Add(47.5);
+        lapTimes.Add(52.6);
+        lapTimes.Add(45.7);
+
+        double fastestLap = lapTimes.Max();
+        double slowestLap = lapTimes.Min();
+
+        Console.WriteLine($"Your fastest lap was {fastestLap} seconds");
+        Console.WriteLine($"Your slowest lap was {slowestLap} seconds");
+    }
+}
